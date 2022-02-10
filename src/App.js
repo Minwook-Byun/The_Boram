ccb8f66ee44610ad08c4321320b8d23b2610fd27
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-10 09:56:44 +0900

    ADD : Add basic route & page setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Create from "./pages/Create/Create";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Groups from "./pages/Groups/Groups";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <div className=".container">
+          <Switch>
+            <Route exact path="/">
+              <Dashboard />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/groups/:id">
+              <Groups />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App
+export default App;
---
+++ src/pages/Groups/Groups.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Groups = () => {
+  return <div>그룹s</div>;
+};
+
+export default Groups;
