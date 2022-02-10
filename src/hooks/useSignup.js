bfd6634772e0376ec716a6609ea8d9769164f17a
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-10 22:16:27 +0900

    구현 : useSignup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,9 @@
 import { useState, useEffect } from "react";
-import { projectAuth } from "../firebase/config";
+import {
+  projectAuth,
+  projectStorage,
+  projectFirestore,
+} from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
@@ -8,7 +12,7 @@ export const useSignup = () => {
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
-  const signup = async (email, password, displayName) => {
+  const signup = async (email, password, displayName, thumbnail) => {
     setError(null);
     setIsPending(true);
 
@@ -19,10 +23,25 @@ export const useSignup = () => {
       );
 
       if (!res) {
-        throw new Error("Could not complete signup");
+        throw new Error("회원가입을 완료하지 못했습니다");
       }
-      await res.user.updateProfile({ displayName });
 
+      // 썸네일 이미지 업로드 섬네일s에 유저의 id마다 폴더를 만들어주고 파일의 이름을 통해서 저장해준다.
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
+      const img = await projectStorage.ref(uploadPath).put(thumbnail);
+      const imgUrl = await img.ref.getDownloadURL();
+
+      // 사용자 이름 업데이트
+      await res.user.updateProfile({ displayName, photoURL: imgUrl });
+
+      // 유저용 document 만들기
+      await projectFirestore.collection("users").doc(res.user.uid).set({
+        online: true,
+        displayName: displayName,
+        photoURL: imgUrl,
+      });
+
+      // 로그인 액션 뿌려주기
       dispatch({ type: "LOGIN", payload: res.user });
 
       if (!isCancelled) {
@@ -32,6 +51,7 @@ export const useSignup = () => {
     } catch (err) {
       if (!isCancelled) {
         setError(err.message);
+        console.log(err.message);
         setIsPending(false);
       }
     }
