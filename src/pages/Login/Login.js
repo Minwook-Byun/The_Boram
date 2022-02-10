5c765fa7a353b86996191184464642578870199f
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-10 22:14:55 +0900

    구현 : Login 페이지 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,47 @@
-import React from "react";
+import { useState } from "react";
+import { useLogin } from "../../hooks/useLogin";
 
 const Login = () => {
-  return <div>로긴~</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, isPending, error } = useLogin();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <div>
+      <form className="auth-form" onSubmit={handleSubmit}>
+        <h2>로그인</h2>
+        <label>
+          <span>이메일: </span>
+          <input
+            required
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </label>
+        <label>
+          <span>비밀번호: </span>
+          <input
+            required
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </label>
+        {isPending ? (
+          <button className="btn">진행중...</button>
+        ) : (
+          <button className="btn">로그인</button>
+        )}
+        {error ? <div className="error">{error}</div> : null}
+      </form>
+    </div>
+  );
 };
 
 export default Login;
