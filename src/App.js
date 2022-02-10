698295a89edd790a48a66f5aca7d04c4a906902d
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-10 19:20:45 +0900

    추가 : 사이드바, 네비게이션 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
+// 페이지
 import Create from "./pages/Create/Create";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Groups from "./pages/Groups/Groups";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 
+// 컴포넌트
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Sidebar />
         <div className=".container">
+          <Navbar />
           <Switch>
             <Route exact path="/">
               <Dashboard />
