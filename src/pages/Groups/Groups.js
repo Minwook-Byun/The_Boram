6f62fed3c9ff5700d48cefc3eed00beb7b066133
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-13 20:23:39 +0900

    구현 : group 컴포넌트 구현 & css 스타일

--- src/pages/Groups/Groups.js
+++ src/pages/Groups/Groups.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+
+import "./Groups.css";
+
+// 컴포넌트
+import GroupSummary from "./GroupSummary";
+import GroupComments from "./GroupComments";
 
 const Groups = () => {
-  return <div>그룹s</div>;
+  const { id } = useParams();
+  const { document, error } = useDocument("projects", id); //위에서 param 으로 받은 id
+
+  // conditional 하는 방법 2 여기서 return이 일어나서 더 이상 내려가지 않는다.
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">로딩중...</div>;
+  }
+
+  return (
+    <div className="project-details">
+      {document ? (
+        <>
+          {/* 안된다면 url도 꼭 살펴보자!  */}
+          <GroupSummary project={document} />
+          <GroupComments project={document} />
+        </>
+      ) : (
+        <p>데이터 로딩중</p>
+      )}
+    </div>
+  );
 };
 
 export default Groups;
