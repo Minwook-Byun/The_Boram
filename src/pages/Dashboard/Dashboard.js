f7e76ec4b6a4c91ef0e11b1eed78820b2b88ee41
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-13 20:27:39 +0900

    추가 : 대쉬보드 페이지 상세 구현

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import { useCollection } from "../../hooks/useCollection";
+import ProjectList from "../../components/ProjectList";
+import "./Dashboard.css";
+
 const Dashboard = () => {
-  return <div>대쉬보드</div>;
+  const { documents, error } = useCollection("projects");
+
+  return (
+    <div>
+      <h2 className="page-title">대쉬보드</h2>
+      {error ? <p>{error}</p> : null}
+      {documents ? <ProjectList projects={documents} /> : null}
+    </div>
+  );
 };
 
 export default Dashboard;
