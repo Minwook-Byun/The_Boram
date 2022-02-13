3abf14c6da1583b690c3e88a375b4febbfc90fac
Boram.minwook <87590768+Minwook-Byun@users.noreply.github.com>
2022-02-13 20:29:43 +0900

    구현 : App 내부 컴포넌트별  conditional rendering구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
 // 페이지
 import Create from "./pages/Create/Create";
 import Dashboard from "./pages/Dashboard/Dashboard";
@@ -10,33 +11,47 @@ import Signup from "./pages/Signup/Signup";
 // 컴포넌트
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import userList from "./components/UserList";
+
+// hooks
+import { useAuthContext } from "./hooks/useAuthContext";
+import Userlist from "./components/UserList";
 
 function App() {
+  const { user, authIsReady } = useAuthContext();
+  // authIsReady action의 타입을 통해서 조건부 렌더링해줄 예정
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Sidebar />
-        <div className=".container">
-          <Navbar />
-          <Switch>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/groups/:id">
-              <Groups />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/signup">
-              <Signup />
-            </Route>
-          </Switch>
-        </div>
-      </BrowserRouter>
+      {authIsReady && (
+        <BrowserRouter>
+          <Sidebar />
+          <div className="container">
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                {user ? <Dashboard /> : <Redirect to="/login" />}
+              </Route>
+              <Route path="/create">
+                {user ? <Create /> : <Redirect to="/login" />}
+              </Route>
+              <Route path="/groups/:id">
+                {user ? <Groups /> : <Redirect to="/login" />}
+              </Route>
+              <Route path="/login">
+                {user ? <Redirect to="/" /> : <Login />}
+              </Route>
+              <Route path="/signup">
+                {user ? <Redirect to="/" /> : <Signup />}
+              </Route>
+              <Route path="/signup">
+                {user ? <Redirect to="/" /> : <Signup />}
+              </Route>
+            </Switch>
+          </div>
+          {user && <Userlist />}
+        </BrowserRouter>
+      )}
     </div>
   );
 }
